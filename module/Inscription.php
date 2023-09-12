e0f5ec85ab66460d29ad592c3ea7efa1cca54a97
Souleimane-ifqirne <ifqirne.souleimane@gmail.com>
2023-09-12 14:16:37 +0200

    feat(Sign in): Added error management : -Login already existing, Login longer than 3 . -email already existing. -password require a good length, 1 digit,     1 upercase, 1 lowercase, 1 special character. -email and password confirm

--- module/Inscription.php
+++ module/Inscription.php
@@ -7,19 +7,98 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user = new User(false, $_POST['login'], $_POST['firstname'], $_POST['lastname'], 
     $_POST['email'], $_POST['emailCheckout'], $_POST['password'], $_POST['passwordCheckout']);
 
+    // || connect to databse
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    // || Array of user "must have" to register
+    $errorMessages = array(
+        'login_length' => "Le nom d'utilisateur doit contenir au moins 3 caractères.",
+        'empty_fields' => "Veuillez remplir tous les champs.",
+        'password_mismatch' => "Les mots de passe ne correspondent pas.",
+        'password_length' => "Le mot de passe est trop court. Veuillez en choisir un plus long.",
+        'password_case' => "Le mot de passe doit contenir au moins une lettre majuscule et une minuscule.",
+        'password_digit' => "Le mot de passe doit contenir au moins un chiffre.",
+        'password_special_char' => "Le mot de passe doit contenir au moins un caractère spécial.",
+        'login_exists' => "Le nom d'utilisateur est déjà utilisé. Veuillez en choisir un autre.",
+        'email_mismatch' => "Les adresses email ne correspondent pas.",
+        'email_invalid' => "L'adresse e-mail doit être au format example@example.com.",
+        'email_exists' => "Cette adresse e-mail est déjà associée à un compte. Veuillez en choisir une autre."
+    );
+
     $errors = [];
 
+    // || verify if login is longer than 3
+    if (strlen($user->getLogin()) < 3) {
+        $errors[] = $errorMessages['login_length'];
+    }
+    
+    // || verify if fields are empy
+    if (empty($user->getLogin()) || empty($user->getEmail()) || empty($user->getEmailCheckout()) || empty($user->getPassword()) || empty($user->getPasswordCheckout())) {
+        $errors[] = $errorMessages['empty_fields'];
+    }
+    
+    // || verify if password mastch the password confirmation
+    if ($user->getPassword() !== $user->getPasswordCheckout()) {
+        $errors[] = $errorMessages['password_mismatch'];
+    }
+    
+    // || verify if password lenght is long enough
+    if (strlen($user->getPassword()) < 8) {
+        $errors[] = $errorMessages['password_length'];
+    }
+    
+    // || verify if password got 1 lowercase and upercase
+    if (!preg_match('/[A-Z]/', $user->getPassword()) || !preg_match('/[a-z]/', $user->getPassword())) {
+        $errors[] = $errorMessages['password_case'];
+    }
+    
+    // || verify if password has at least one digit
+    if (!preg_match('/[0-9]/', $user->getPassword())) {
+        $errors[] = $errorMessages['password_digit'];
+    }
+    
+    // || verify if password has at least one special characters
+    if (!preg_match('/[^A-Za-z0-9]/', $user->getPassword())) {
+        $errors[] = $errorMessages['password_special_char'];
+    }
+
+        // || verify if the login is already used
+    $stmt = $db->prepare('SELECT * FROM user WHERE login = :login');
+    $stmt->execute([':login' => $user->getLogin()]);
+    $count = $stmt->fetchColumn();
+
+    if ($count > 0) {
+        $errors[] = $errorMessages['login_exists'];
+    } $stmt = null;
+
+    // || verify if email match with email confirmation
+    if (strcasecmp($user->getEmail(), $user->getEmailCheckout()) !== 0) {
+        $errors[] = $errorMessages['email_mismatch'];
+    }
+
+    if (!filter_var($user->getEmail(), FILTER_VALIDATE_EMAIL)) {
+        $errors[] = $errorMessages['email_invalid'];
+    } else {
+        $stmt = $db->prepare('SELECT * FROM user WHERE email = :email');
+        $stmt->execute([':email' => $user->getEmail()]);
+        $count = $stmt->fetchColumn();
+        if ($count > 0) {
+            $errors[] = $errorMessages['email_exists'];
+        } $stmt = null;
+    }
+    // || checkout every error possible and Insert ser information into database
     if (empty($errors)) {
         $hashedPassword = $user->hashPassword();
         $userData = [$user->getLogin(), $user->getFirstname(), $user->getLastname(), $user->getEmail(), $hashedPassword];
-        $stmt = $db->prepare("INSERT INTO user (login, email, firstname, lastname, password) VALUES (:login, :email, :firstname, :lastname, :password)");
+        $stmt = $db->prepare('INSERT INTO user (login, email, firstname, lastname, password) VALUES (:login, :email, :firstname, :lastname, :password)');
         if ($stmt->execute([':login' => $userData[0], ':firstname' => $userData[1], ':lastname' => $userData[2],':email' => $userData[3], ':password' => $userData[4]])) {
             $success = "Inscription résussie !";
         } else {
             $errors[] = "Erreur lors de l'inscription : " . $stmt->errorInfo()[2];
         }
-            $stmt = null;
-        }
+    }
     $db = null;
 }
     
